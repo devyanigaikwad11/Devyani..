e355990f2cdcf366fe39b6b063f98176e2031eb8
devyanigaikwad11 <devyanigaikwad11@gmail.com>
2026-02-16 14:51:04 +0530

    Create Dev.java

---
+++ Dev.java
@@ -0,0 +1,7 @@
+// Java Program with Dev class name
+
+public class Dev {                 // Class name is Dev
+    public static void main(String[] args) {   // Main method
+        System.out.println("Welcome to Java Programming!");
+    }
+}
