4a3295dddf961c75383eef3bc6d79ec36059fc9b
devyanigaikwad11 <devyanigaikwad11@gmail.com>
2026-02-16 14:37:18 +0530

    Create dev.cpp

---
+++ dev.cpp
@@ -0,0 +1,7 @@
+#include <iostream>   // Header file for input and output
+using namespace std;  // Allows us to use standard names like cout
+int main()            // Main function (program starts here)
+{
+    cout << "Hello, World!";   // Output statement
+    return 0;                  // End of program
+}
