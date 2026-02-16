ab317ff25a74780ce09b212c788d6d67047cbf74
devyanigaikwad11 <devyanigaikwad11@gmail.com>
2026-02-16 14:40:26 +0530

    Create dev.java

---
+++ dev.java
@@ -0,0 +1,6 @@
+// Basic Java Program
+public class Main {          // Class name
+    public static void main(String[] args) {   // Main method
+        System.out.println("Hello, World!");   // Print statement
+    }
+}
